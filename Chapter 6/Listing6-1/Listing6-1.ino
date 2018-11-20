93527f7549503ea03671fad60b2b135527f52d54
NDC-SC <38754611+NDC-SC@users.noreply.github.com>
2018-11-20 21:23:22 +0000

    Add files via upload

---
+++ Chapter 6/Listing6-1/Listing6-1.ino
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Sketch name: 4-digit 7-segment display as timer
+ * Description: simple use of 4-digit 7-segment display as timer
+ * Created :    October 2018
+ * Author:      Neil Cameron
+ * Book:        Applied Arduino: Comprehensive Projects for Everyday Electronics
+ * Chapter :    Chapter 6 - 4-digit 7-segment LED display
+ ******************************************************************************/
+
+int pins[ ] = {2,3,4,5,6,7,8};        // LED pins
+int digits[ ] = {10,11,12,13};        // digit control pins
+int zero[ ] ={1,1,1,1,1,1,0};       // LED states for zero
+int one[ ] = {0,1,1,0,0,0,0};       // LED states for one
+int two[ ] = {1,1,0,1,1,0,1};
+int three[ ] = {1,1,1,1,0,0,1};
+int four[ ] = {0,1,1,0,0,1,1};
+int five[ ] = {1,0,1,1,0,1,1};
+int six[ ] = {1,0,1,1,1,1,1};
+int seven[ ] = {1,1,1,0,0,0,0};
+int eight[ ] = {1,1,1,1,1,1,1};
+int nine[ ] = {1,1,1,1,0,1,1};
+int time, n;
+int del = 5;          // time delay (ms)
+
+void setup()
+{
+  for (int i = 0; i<7; i++) pinMode (pins[i], OUTPUT);              // define pins and digits as output
+  for (int i = 0; i<4; i++) pinMode (digits[i], OUTPUT);
+}
+
+void loop()
+{
+  time = millis()/1000;       // time is number of seconds
+  digit(0);           // digit D1 for thousands
+  number(time/1000);        // number to be displayed
+  delay(del);
+  digit(1);           // digit D2 for hundreds
+  number((time%1000)/100);        // modulus(time, 1000)/100
+  delay(del);
+  digit(2);           // digit D3 for tens
+  number((time%100)/10);        // modulus(time, 100)/10
+  delay(del);
+  digit(3);           // digit D4 for units
+  number(time%10);          // modulus(time, 10)
+  delay(del);
+}
+
+void digit(int d)
+{
+  for (int i = 0; i<4; i++) digitalWrite(digits[i], 1);   // turn all digits off
+  digitalWrite(digits[d], 0);                  // digit pin state is LOW, digit display is on
+}
+
+void number(int n)
+{
+  if        (n==0) for (int i = 0; i<7; i++) digitalWrite(pins[i], zero[i]);             // display zero
+  else if (n==1) for (int i = 0; i<7; i++) digitalWrite(pins[i], one[i]);            // display one
+  else if (n==2) for (int i = 0; i<7; i++) digitalWrite(pins[i], two[i]);            // display two
+  else if (n==3) for (int i = 0; i<7; i++) digitalWrite(pins[i], three[i]);
+  else if (n==4) for (int i = 0; i<7; i++) digitalWrite(pins[i], four[i]);
+  else if (n==5) for (int i = 0; i<7; i++) digitalWrite(pins[i], five[i]);
+  else if (n==6) for (int i = 0; i<7; i++) digitalWrite(pins[i], six[i]);
+  else if (n==7) for (int i = 0; i<7; i++) digitalWrite(pins[i], seven[i]);
+  else if (n==8) for (int i = 0; i<7; i++) digitalWrite(pins[i], eight[i]);
+  else if (n==9) for (int i = 0; i<7; i++) digitalWrite(pins[i], nine[i]);
+}
+
---
+++ Chapter 6/Listing6-4/Listing6-4.ino
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Sketch name: temperature and time display
+ * Description: control temperature and time display with one 74HC595 shift register
+ * Created :    October 2018
+ * Author:      Neil Cameron
+ * Book:        Applied Arduino: Comprehensive Projects for Everyday Electronics
+ * Chapter :    Chapter 6 - 4-digit 7-segment LED display
+ ******************************************************************************/
+
+int dataPin = 2;          // shift register DATA pin
+int latchPin = 3;         // shift register LATCH pin
+int clockPin = 4;         // shift register CLOCK pin
+int digits[ ] = {10,11,12,13};        // four digit pins
+int numbers[ ] = {63,6,91,79,102,109,125,7,127,111};  // "values" of numbers 0 to 9
+int del = 5;          // delay after turning digit on
+int tempPin = A5;         // temperature sensor pin
+int duration = 5000;          // display duration
+unsigned long start;
+int time, n, temp, reading;
+
+void setup()
+{
+  pinMode (dataPin, OUTPUT);               // define shift register DATA pin as output
+  pinMode (latchPin, OUTPUT);              // define shift register LATCH pin as output
+  pinMode (clockPin, OUTPUT);              // define shift register CLOCK pin as output
+  for (int i = 0; i<4; i++) pinMode (digits[i], OUTPUT);  // four digit pins as output
+  analogReference (INTERNAL);   // set ADC voltage to 1.1V rather than 5V
+}
+
+void loop()
+{
+  start = millis();       // milliseconds elapsed
+  while (millis()-start<duration)     // first duration, display time
+  {
+    time = millis()/1000;     // time in elapsed seconds
+    digit(0, time/1000, 0);     // digit D1 for thousands
+    digit(1, (time%1000)/100, 0);   // digit D2 for hundreds
+    digit(2, (time%100)/10, 0);     // digit D3 for tens
+    digit(3, time%10, 0);     // digit D4 for units
+  }
+  reading = analogRead(tempPin);    // temperature reading
+  temp = 10.0*(reading*110.0)/1023.0;   // multiplier to get decimal place
+          // second duration, display temperature
+  while (millis()-start>duration && millis()-start<2*duration)
+  {
+    digit(1, (temp%1000)/100, 0);   // digit D2 for tens
+    digit(2, (temp%100)/10, 1);     // digit D3 for units 1 for DP
+    digit(3, temp%10, 0);     // digit D4 for decimal places
+  }
+}
+
+void digit(int d, int n, int DP)
+{
+  for (int i = 0; i<4; i++) digitalWrite(digits[i], 1);   // turn all digits off, digit states are HIGH
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, numbers[n]+DP*128); // add 128 for DP
+  digitalWrite(latchPin, HIGH);       // change display pattern
+  digitalWrite(digits[d], 0);       // turn digit on, digit state LOW
+  delay(del);         // delay del (ms)
+}
+
