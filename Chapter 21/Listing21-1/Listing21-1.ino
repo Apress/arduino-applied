e8bf6296257efdaa3f535e1f42a24834427b7142
NDC-SC <38754611+NDC-SC@users.noreply.github.com>
2018-11-20 21:28:22 +0000

    Add files via upload

---
+++ Chapter 21/Listing21-1/Listing21-1.ino
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Sketch name: sleep mode with avr/sleep module
+ * Description: microcontroller in sleep mode with interrupt to "wake"
+ * Created :    October 2018
+ * Author:      Neil Cameron
+ * Book:        Applied Arduino: Comprehensive Projects for Everyday Electronics
+ * Chapter :    Chapter 21 - Power saving
+ ******************************************************************************/
+
+#include <avr/sleep.h>      // include avr/sleep library
+int LEDpin = 11;
+int wakePin = 2;        // pin connected to interrupt 0
+
+void setup()
+{
+  pinMode(LEDpin, OUTPUT);      // LED pin as OUTPUT
+  pinMode(LED_BUILTIN, OUTPUT);   // turn off built-in LED
+}
+
+void loop()
+{
+  delay(500);
+  digitalWrite(LEDpin, LOW);      // turn LED on and off after sleep mode
+  sleep();          // function to set sleep mode
+}
+
+void sleep()
+{
+  attachInterrupt(0, wake, RISING);   // interrupt wake function
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // define sleep mode
+ //sleep_enable();        // set sleep enable bit
+ //sleep_cpu();       // initiate sleep
+  sleep_mode();              // set sleep enable bit, initiate sleep and reset
+  // sketch resumes here when interrupt triggered
+ //sleep_disable();       // reset sleep enable bit
+  detachInterrupt(0);       // effectively debounces switch interrupt
+}
+
+void wake()       // wake interrupt function
+{
+  digitalWrite(LEDpin, HIGH);      // turn LED on
+}
+
---
+++ Chapter 21/Listing21-3/Listing21-3.ino
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Sketch name: power saving with PIR sensor interrupt
+ * Description: microcontroller in sleep mode with motion activated interrupt to wake
+ * Created :    October 2018
+ * Author:      Neil Cameron
+ * Book:        Applied Arduino: Comprehensive Projects for Everyday Electronics
+ * Chapter :    Chapter 21 - Power saving
+ ******************************************************************************/
+
+#include <LowPower.h>      // include LowPower library
+int LEDpin = 11;        // LED pin
+int PIRpin = 2;         // interrupt on pin 2 (INT0 or interrupt 0)
+int detect = 0;       // movement flag
+
+void setup()
+{
+//  Serial.begin(9600);     // Serial output baud rate
+  pinMode(LEDpin, OUTPUT);      // LED pin as output
+//  pinMode(LED_BUILTIN, OUTPUT);     // turn off built-in LED
+}
+
+void loop()
+{
+  if(detect == 1)       // if movement detected
+  {
+    delay(3000);       // delay 30s as LED is ON
+    digitalWrite(LEDpin, LOW);      // turn LED off
+    delay(1000);        // delay 1s
+  }
+  detect = 0;       // reset movement flag
+  digitalWrite(LEDpin, LOW);      // turn LED off
+  for (int i = 0; i<4; i++)
+  {           // four steps for turning LED
+    digitalWrite(LEDpin, !digitalRead(LEDpin));     //      on and off twice
+    delay(1000);          // LED on or off for 1s
+  }
+  sleep();          // call sleep function
+}
+
+void sleep()        // sleep function
+{
+//  Serial.println("sleep");      // print message to Serial Monitor
+//  delay(10);        // time required to print before sleep
+  attachInterrupt(0, wake, RISING);     // interrupt pin, wake function and mode
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);            // power down
+  detachInterrupt(0);       // detach interrupt
+}
+
+void wake()       // wake interrupt function
+{
+//  Serial.println("Movement detected");    // print message to Serial Monitor
+  digitalWrite(LEDpin, HIGH);     // turn LED on
+  detect = 1;       // set movement flag
+}
+
