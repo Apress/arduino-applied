7340a778da520832a57796f868b8d6e5bb54cc36
NDC-SC <38754611+NDC-SC@users.noreply.github.com>
2018-11-20 21:20:51 +0000

    Add files via upload

---
+++ Chapter 1/Listing1-2/Listing1-2.ino
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Sketch name: LED brightness and PWM
+ * Description: LED brightness changed by PWM with controlled rate of change 
+ * Created on:  October 2018
+ * Author:      Neil Cameron
+ * Book:        Applied Arduino: Comprehensive Projects for Everyday Electronics
+ * Chapter :    Chapter 1 - Introduction
+ ******************************************************************************/
+
+int LEDpin = 11;      // define LED pin
+int bright = 0;       // initial value for LED brightness
+int increm = 5;       // incremental change in PWM frequency
+int time = 25;        // define time period between changes
+
+void setup()        // setup function runs once
+{
+  pinMode(LEDpin, OUTPUT);      // LED pin as output
+}
+
+void loop()       // loop function runs continuously
+{
+  analogWrite(LEDpin, bright);      // set LED brightness with PWM
+  delay(time);        // wait for the time period
+  bright = bright + increm;     // increment LED brightness
+           // reverse increment at brightness = 0 or 255
+  if(bright <=0 || bright >= 255) increm = - increm;
+}
+
+
